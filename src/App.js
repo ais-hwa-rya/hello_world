d125c46a8575ecd1c9a3385cf2313593365825b7
Aishwarya R <61188154+ais-hwa-rya@users.noreply.github.com>
2024-07-18 08:43:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
